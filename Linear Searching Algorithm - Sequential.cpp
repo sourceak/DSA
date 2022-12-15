b32eead3186ab275c523a3cf94b6d7ed29d1d903
Akaytop <akaykinfe@gmail.com>
2022-12-15 12:23:45 -0500

    Linear Searching Algorithm - Sequential.cpp

---
+++ Linear Searching Algorithm - Sequential.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void linearSearch(int a[], int num){
+
+	for(int i=0; i<5; i++){
+		if(a[i]==num){
+			cout<<num<<" found at index position "<<i<<endl;
+			break;
+		}else if(4==i){
+			cout<<"Value "<<num<<" is unable to be located"<<endl;
+		}
+	}
+}
+
+
+int main(){
+
+	int arr[5];
+	cout<<"Enter 5 elements for the array"<<endl;
+
+	for(int i=0; i<5; ){
+
+	}
+
+	cout<<"Enter a value to search"<<endl;
+
+	int x;
+	cin>>x;
+
+	linearSearch(arr,x);
+
+	return 0;
+}
